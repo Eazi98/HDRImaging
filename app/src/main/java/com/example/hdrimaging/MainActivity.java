2f5fa6c4e20c1c714f65afd0fee4b7c7e92e947c
Eazi98 <64185008+Eazi98@users.noreply.github.com>
2022-12-12 23:21:28 +0800

    Added library for reading hdr file

--- app/src/main/java/com/example/hdrimaging/MainActivity.java
+++ app/src/main/java/com/example/hdrimaging/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.hdrimaging;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.ColorSpace;
+import android.graphics.ImageDecoder;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Handler;
 import android.provider.Settings;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.activity.result.ActivityResult;
@@ -23,20 +28,23 @@ import com.example.hdrimaging.databinding.ActivityMainBinding;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.lang.reflect.Array;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView fileLocationText;
+    private ImageView imageHDR;
     private TextView arrayText;
+    private ScrollView scrollView;
     private HDRtofloatarray hdrtofloatarray;
-    private Boolean hdrLoaded;
-    private String pixelArrayText;
-    private float[][][] pixelArray;
+    private ArrayList<Float> HDRArray;
+    private ArrayList<Float> pixelArrayTextArray = new ArrayList<>();
+    private int loadArray = 200;
+    int arraySize;
+
+
     ActivityResultLauncher<Intent> filePicker;
     Thread arrayThread = null;
     @Override
@@ -46,8 +54,9 @@ public class MainActivity extends AppCompatActivity {
         com.example.hdrimaging.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         fileLocationText = binding.FileLocationText;
+        imageHDR = binding.imageView;
         arrayText = binding.arrayText;
-        arrayText.setMovementMethod(new ScrollingMovementMethod());
+        scrollView = binding.scrollView;
         if (Build.VERSION.SDK_INT >= 30) {
             if (!Environment.isExternalStorageManager()) {
                 Intent getpermission = new Intent();
@@ -59,7 +68,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 //                ChooseFile();
+                arrayText.setText(null);
                 openFileDialog(view);
+
+            }
+        });
+
+        binding.scrollView.setOnScrollChangeListener(new View.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(View view, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                if (scrollView.getChildAt(0).getBottom() <= (scrollView.getHeight() + scrollView.getScrollY())) {
+                    for (int i= 0; i < loadArray; i++){
+                        arrayText.append(" "+ String.format("%.4f", HDRArray.get(0)));
+                        HDRArray.remove(0);
+                    }
+                }
+//                if (scrollY > oldScrollY + 100){
+//
+//                }
 
             }
         });
@@ -68,6 +94,7 @@ public class MainActivity extends AppCompatActivity {
     ActivityResultLauncher<Intent> sActivityResultLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
+                @SuppressLint("SetTextI18n")
                 @Override
                 public void onActivityResult(ActivityResult result) {
                     if (result.getResultCode() == Activity.RESULT_OK){
@@ -76,9 +103,20 @@ public class MainActivity extends AppCompatActivity {
                         Uri uri = data.getData();
                         String path = getFileName(uri, getApplicationContext());
                         fileLocationText.setText(path);
-                        pixelArrayText = getHDRArray(path, getApplicationContext());
-                        arrayText.setText(pixelArrayText);
+//                        try {
+//                            Drawable drawable = showHDR(path);
+//                            imageHDR.setImageDrawable(drawable);
+//                        } catch (IOException e) {
+//                            e.printStackTrace();
+//                        }
 
+                        HDRArray = getHDRArray(path, getApplicationContext());
+
+                        arrayText.setText(String.format("%.4f",HDRArray.get(0)));
+                        for (int i= 1; i < loadArray; i++){
+                            arrayText.append(" "+ String.format("%.4f", HDRArray.get(0)));
+                            HDRArray.remove(0);
+                        }
                     }
                 }
             }
@@ -89,6 +127,7 @@ public class MainActivity extends AppCompatActivity {
         data.setType("*/*");
         data = Intent.createChooser(data, "Choose a file");
         sActivityResultLauncher.launch(data);
+        //arrayText.append(pixelArrayText);
         if (fileLocationText.getText() == ""){
             Snackbar.make(view, "Do not select files from recent!", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show();
@@ -101,36 +140,55 @@ public class MainActivity extends AppCompatActivity {
         return res;
     }
 
-    String getHDRArray (String path, Context context){ //TODO: might need to put in threaded function
+    ArrayList<Float> getHDRArray (String path, Context context){ //TODO: might need to put in threaded function
+        pixelArrayTextArray.clear();
         File file = new File(path); //for HDRtofloatarray
-        pixelArrayText = "";
+//        pixelArrayText = "";
         try {
             hdrtofloatarray = new HDRtofloatarray(file);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        pixelArray = hdrtofloatarray.getPixelArray();
+        float[][][] pixelArray = hdrtofloatarray.getPixelArray();
         int width = hdrtofloatarray.getWidth();
         int length = hdrtofloatarray.getHeight();
 
         //For testing
-        for (int i = length - 1; i >= 110; i--)
-            for (int j = width - 1; j >= 169; j--)
-                for (int k = 0; k < 3; k++)
-                    pixelArrayText += pixelArray[i][j][k] + "\t";
-
-        //Full array
-//        for (int i= 0; i < pixelArray.length; i++)
-//            for (int j=0; j < pixelArray[i].length; j++)
-//                for (int k=0; k < pixelArray[i][j].length; k++)
+//        for (int i = length - 1; i >= 110; i--)
+//            for (int j = width - 1; j >= 169; j--)
+//                for (int k = 0; k < 3; k++)
 //                    pixelArrayText += pixelArray[i][j][k] + "\t";
 
+//        Full array
+//        for (int i = 0; i < pixelArray.length; i++)
+//            for (int j = 0; j < pixelArray[i].length; j++)
+//                for (int k = 0; k < pixelArray[i][j].length; k++){
+////                    pixelArrayText += pixelArray[i][j][k] + "\t";
+//                    pixelArrayTextArray.add(pixelArray[i][j][k]);
+//                }
+
+
         //Full array
-//        for (float[][] floats : pixelArray)
-//            for (float[] aFloat : floats)
-//                for (float v : aFloat) pixelArrayText += v + "\t";
+        for (float[][] floats : pixelArray)
+            for (float[] aFloat : floats)
+                for (float v : aFloat) pixelArrayTextArray.add(v);
         //TODO: Convert pixelArray to a readable string
-        return pixelArrayText;
+        return pixelArrayTextArray;
     }
+
+//    Drawable showHDR(String path) throws IOException {
+//        Drawable drawable = null;
+//        File file = new File(path);
+//        ImageDecoder.Source source = null;
+//        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {
+//            source = ImageDecoder.createSource(file);
+//        }
+//        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {
+//            drawable = ImageDecoder.decodeDrawable(source, (decoder, info, src) -> {
+//                decoder.setTargetColorSpace(ColorSpace.get(ColorSpace.Named.EXTENDED_SRGB));
+//            });
+//        }
+//        return drawable;
+//    }
 
 }
