3427556b979711747f3196252949f82de60a1174
Eazi98 <64185008+Eazi98@users.noreply.github.com>
2023-05-02 22:47:19 +0800

    Removed unused files

--- MATLABImplementationOfImageTMO/HDRImaging_ert_rtw/HDRImaging/src/main/java/com/example/HDRImaging/OnFragmentInteractionListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.HDRImaging;
-
-
-interface OnFragmentInteractionListener {
-   void onFragmentCreate (String name);
-   void onFragmentStart  (String name);
-   void onFragmentPause  (String name);
-   void onFragmentResume (String name);
-}
