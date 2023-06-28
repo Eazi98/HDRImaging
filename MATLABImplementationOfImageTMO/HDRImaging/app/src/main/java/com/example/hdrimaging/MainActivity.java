1b2f87957720a6586dc4d90a66b39c9abb4a5aa0
Eazi98 <64185008+Eazi98@users.noreply.github.com>
2023-06-28 22:28:25 +0800

    Added support for SDK 29

--- MATLABImplementationOfImageTMO/HDRImaging/app/src/main/java/com/example/hdrimaging/MainActivity.java
+++ MATLABImplementationOfImageTMO/HDRImaging/app/src/main/java/com/example/hdrimaging/MainActivity.java
@@ -5,19 +5,22 @@ import static com.example.hdrimaging.DCA_TMO.DCA_TMO_Processing;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ActivityManager;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.Settings;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.activity.result.ActivityResult;
@@ -32,7 +35,9 @@ import com.google.android.material.snackbar.Snackbar;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -61,22 +66,25 @@ public class MainActivity extends AppCompatActivity {
         ldrFilePath = binding.LDRText;
         timeTaken = binding.TimeTakenText;
         timeTakenOverAll = binding.TimeTakenOverallText;
-        if (Build.VERSION.SDK_INT >= 30) {
-            if (!Environment.isExternalStorageManager()) {
-                Intent getpermission = new Intent();
-                getpermission.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
-                startActivity(getpermission);
-            }
-        }
+//        if (Build.VERSION.SDK_INT >= 30) {
+//            try{
+//                if (!Environment.isExternalStorageManager()) {
+//                    Intent getPermission = new Intent();
+//                    getPermission.setAction(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+//                    startActivity(getPermission);
+//                }
+//            }catch(Exception e){
+//                Toast.makeText(this, "Allow permission for storage access!", Toast.LENGTH_LONG).show();
+//            }
+//
+//        }
         ActivityManager am = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
         int memoryClass = am.getMemoryClass();
         Log.v("onCreate", "memoryClass:" + Integer.toString(memoryClass) + "MB");
         binding.ReadFileButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 openFileDialog(view);
-
             }
         });
 
@@ -87,7 +95,13 @@ public class MainActivity extends AppCompatActivity {
         Intent data = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         data.setType("*/*");
         data = Intent.createChooser(data, "Choose a file");
-        sActivityResultLauncher.launch(data);
+        try{
+            sActivityResultLauncher.launch(data);
+        }catch(IllegalArgumentException e){
+            Snackbar.make(view, "Incorrect File Type/ File size too large", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }
+
         if (fileLocationText.getText() == ""){
             Snackbar.make(view, "Do not select files from recent!", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show();
@@ -104,37 +118,83 @@ public class MainActivity extends AppCompatActivity {
                         Intent data = result.getData();
                         assert data != null;
                         Uri uri = data.getData();
+                        String test = copyFileToInternalStorage(uri, "test");
                         String path = getFileName(uri, getApplicationContext());
                         fileLocationText.setText(path);
-//                        long startTime = System.nanoTime();
-//                        //HDRDoubleArray = getHDRDoubleArray(path);
-//                        double[][][] LDRDoubleArray = DCATMO(getHDRDoubleArray(path));
-//
-//                        createBitMap(LDRDoubleArray,path);
-//
-//                        long endTime = System.nanoTime();
-//                        long TimeTaken = endTime - startTime;
-//                        timeTakenOverAll.setText(String.valueOf("Time taken including read & write: \n" + nanosecondsToSeconds(TimeTaken)));
-
                         long startTime = System.nanoTime();
-                        create3DArray(length, width, 3);
+                        //HDRDoubleArray = getHDRDoubleArray(path);
+                        double[][][] LDRDoubleArray = null;
+                        LDRDoubleArray = DCATMO(getHDRDoubleArray(test));
+
+
+                        try {
+                            createBitMap(LDRDoubleArray,test);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
                         long endTime = System.nanoTime();
                         long TimeTaken = endTime - startTime;
-                        timeTakenOverAll.setText(String.valueOf("Time C takes to create array: \n" + nanosecondsToSeconds(TimeTaken)));
-
-
-//                        LDRDoubleArray = null;
+                        timeTakenOverAll.setText("Time taken including read & write: \n" + nanosecondsToSeconds(TimeTaken));
+//                        long startTime = System.nanoTime();
+//                        create3DArray(length, width, 3);
+//                        long endTime = System.nanoTime();
+//                        long TimeTaken = endTime - startTime;
+//                        timeTakenOverAll.setText(String.valueOf("Time C takes to create array: \n" + nanosecondsToSeconds(TimeTaken)));
+                        LDRDoubleArray = null;
                     }
                 }
             }
     );
+    private String copyFileToInternalStorage(Uri uri, String newDirName) {
+        Uri returnUri = uri;
 
+        Cursor returnCursor = this.getContentResolver().query(returnUri, new String[]{
+                OpenableColumns.DISPLAY_NAME, OpenableColumns.SIZE
+        }, null, null, null);
+
+        int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+        int sizeIndex = returnCursor.getColumnIndex(OpenableColumns.SIZE);
+        returnCursor.moveToFirst();
+        String name = (returnCursor.getString(nameIndex));
+        String size = (Long.toString(returnCursor.getLong(sizeIndex)));
+
+        File output;
+        if (!newDirName.equals("")) {
+            File dir = new File(this.getFilesDir() + "/" + newDirName);
+            if (!dir.exists()) {
+                dir.mkdir();
+            }
+            output = new File(this.getFilesDir() + "/" + newDirName + "/" + name);
+        } else {
+            output = new File(this.getFilesDir() + "/" + name);
+        }
+        try {
+            InputStream inputStream = this.getContentResolver().openInputStream(uri);
+            FileOutputStream outputStream = new FileOutputStream(output);
+            int read = 0;
+            int bufferSize = 1024;
+            final byte[] buffers = new byte[bufferSize];
+            while ((read = inputStream.read(buffers)) != -1) {
+                outputStream.write(buffers, 0, read);
+            }
+
+            inputStream.close();
+            outputStream.close();
+
+        } catch (Exception e) {
+            Log.e("Exception", e.getMessage());
+        }
+
+        return output.getPath();
+    }
     public double[][][] DCATMO(double[][][] HDRDoubleArray){
         long startTime = System.nanoTime();
         double[][][] retArray = DCA_TMO_Processing(HDRDoubleArray, length, width);
+
         long endTime = System.nanoTime();
         long TimeTaken = endTime - startTime;
-        timeTaken.setText(String.valueOf("Time taken for DCATMO to run: \n" + nanosecondsToSeconds(TimeTaken)));
+        timeTaken.setText("Time taken for DCATMO to run: \n" + nanosecondsToSeconds(TimeTaken));
         return retArray;
     }
 
@@ -160,11 +220,11 @@ public class MainActivity extends AppCompatActivity {
         length = hdrtodoublearray.getHeight();
         return hdrtodoublearray.getPixelArray();
     }
-    private void createBitMap(double[][][] ldrImg, String path){// your 2D array
-        File folder = new File(path).getParentFile();
+    private void createBitMap(double[][][] ldrImg, String path) throws IOException {
+        File folder = new File(String.valueOf(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS+ "/LDRIm")));
         String[] fileNameArray = path.split("/");
-        String fileName = fileNameArray[fileNameArray.length-1];
-        File directory = new File(folder + "/LDRim/");
+        String fileName = fileNameArray[fileNameArray.length-1].split("\\.")[0];
+        File directory = new File(String.valueOf(folder));
         if (!directory.exists()) {
             directory.mkdirs();
         }
@@ -183,16 +243,38 @@ public class MainActivity extends AppCompatActivity {
         // Save the Bitmap object as a PNG file
         fileName = fileName + ".png";
         File file = new File(directory, fileName);
-        try {
-            file.createNewFile();
-            FileOutputStream outputStream = new FileOutputStream(file);
-            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
-            outputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (Build.VERSION.SDK_INT >= 30){
+            try {
+                file.createNewFile();
+                FileOutputStream outputStream = new FileOutputStream(file);
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+                outputStream.close();
+                imageHDR.setImageURI(Uri.parse(file.toString()));
+                ldrFilePath.setText("File Path for LDR .PNG Image: \n" + file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else {
+            OutputStream fos;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                ContentResolver resolver = this.getContentResolver();
+                ContentValues contentValues = new ContentValues();
+                contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, fileName);
+                contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/png");
+                contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS);
+                Uri endpoint =MediaStore.Downloads.EXTERNAL_CONTENT_URI;
+                Uri imageUri = resolver.insert(endpoint, contentValues);
+                fos = resolver.openOutputStream(Objects.requireNonNull(imageUri));
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+                Objects.requireNonNull(fos).close();
+                imageHDR.setImageURI(imageUri);
+                file = new File(String.valueOf(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)));
+                ldrFilePath.setText("File Path for LDR .PNG Image: \n" + file + "/" + fileName);
+            }
         }
-        ldrFilePath.setText("File Path for LDR .PNG Image: \n" + file);
-        imageHDR.setImageURI(Uri.parse(file.toString()));
+
+
     }
+
 
 }
