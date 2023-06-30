1ab9306327caf84c45da1ec59dbafa71d3887a50
Eazi98 <64185008+Eazi98@users.noreply.github.com>
2023-06-30 18:37:39 +0800

    Removed unnecessary file from initial testing

--- MATLABImplementationOfImageTMO/LDRim/HDRImaging_ert_rtw/HDRImaging/src/main/obj/local/armeabi-v7a/objs/HDRImaging/driver_android_toapp.o.d
+++
@@ -1,2 +0,0 @@
-D:\GitRepos\HDRImaging\MATLABImplementationOfImageTMO\LDRim\HDRImaging_ert_rtw\HDRImaging\src\main/obj/local/armeabi-v7a/objs/HDRImaging/driver_android_toapp.o: \
-  ..\driver_android_toapp.c
